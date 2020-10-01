5ea4c4f7035cf5e7298ec65e01d8562fcb49375c
mart vettner <martvettner@gmail.com>
2020-10-01 11:37:30 +0300

    xd

---
+++ Week5Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week5Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!" );
+        }
+    }
+}
